8346610632466ae826e8e95a58d3900eebba6ee8
Riovir <horvatkrisz@gmail.com>
2016-11-12 18:16:18 +0100

    Add support for query configuration
    As of v2.1.0-beta.23 webpack requires loader configuration to be passed in as a query.
    
    By merging the default config instead of using || the loader always considers the defaultConfig when it gets partially set. Due to this change in behavior bumping the major version up.

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 'use strict';
 
 var isparta = require('isparta');
+var loaderUtils = require('loader-utils');
 
 module.exports = function(source) {
-    var config = this.options.isparta || {
+    var query = loaderUtils.parseQuery(this.query);
+    var defaultConfig = {
         embedSource: true,
         noAutoWrap: true,
         babel: this.options.babel
     };
+    var config = Object.assign({}, defaultConfig, this.options.isparta, query);
 
     var instrumenter = new isparta.Instrumenter(config);
 
